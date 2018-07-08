497011304041b4e5fb5823d028bb3b8d1d74051d
fwzt233 <39308322+fwzt233@users.noreply.github.com>
2018-07-08 09:48:50 +0800

    1807080948

---
+++ h5game/post_zc/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1531014500,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"media/m.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js"
+	]
+}
